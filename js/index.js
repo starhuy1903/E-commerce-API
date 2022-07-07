627d0662007b01f832404e42dcbd50c1d1907645
Huy Nguyen <huykst137@gmail.com>
2022-07-07 10:38:27 +0700

    render products list

--- js/index.js
+++ js/index.js
@@ -1,3 +1,34 @@
+let prodsList = [];
+
+const getEle = (id) => document.getElementById(id);
+
+const renderProductsList = (data) => {
+  let contentHTML = "";
+
+  for (let i = 0; i < data.length; i++) {
+    contentHTML += ` <div class="col-lg-3 col-md-6 col-sm-6">
+            <figure class="card card-product-grid">
+              <div class="img-wrap">
+                <img src="${data[i].img}" alt="" style="width: 100%" />
+              </div>
+              <figcaption class="info-wrap border-top p-2">
+                <div class="price-wrap">
+                  <span id="price">$${data[i].price}</span>
+                </div>
+                <div class="name-wrap">
+                    <span id="name">${data[i].name}</span>
+                </div>
+                <p class="description mb-2">
+                  ${data[i].desc}
+                </p>
+                <button class="btn btn-primary mb-1">Add to cart</button>
+              </figcaption>
+            </figure>
+          </div>`;
+  }
+  getEle("product-list").innerHTML = contentHTML;
+};
+
 const getProductsList = async () => {
   let response = await fetch(
     "https://62bdd5cfbac21839b60c0579.mockapi.io/api/phones",
@@ -5,9 +36,11 @@ const getProductsList = async () => {
   );
   let prods = await response.json();
 
-  console.log(prods);
+  if (prods) {
+    prodsList = [...prods];
+    renderProductsList(prodsList);
+    console.log(prods);
+  }
 };
-
-const renderProductsList = () => {};
 
 getProductsList();
